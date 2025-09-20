385201de19bd04c5af34476c9f77c28b4ba2eb83
alejopotes22 <David.potes@uao.edu.co>
2025-09-19 19:52:58 -0500

    Tarea 3

--- New Project/Assets/scripts/Scene2/FruitSpawner.cs
+++ New Project/Assets/scripts/Scene2/FruitSpawner.cs
@@ -1,65 +1,44 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
+﻿using UnityEngine;
 using System.Collections.Generic;
 
 public class FruitSpawner : MonoBehaviour
 {
-    public GameObject applePrefab;
+    public GameObject manzanaPrefab;
     public GameObject bananaPrefab;
 
-    public int maxApples = 5;
+    public int maxManzanas = 5;
     public int maxBananas = 7;
 
-    public Tilemap groundTilemap; // referencia al Tilemap del suelo
 
-    private List<Vector3> groundPositions = new List<Vector3>();
+    public List<GameObject> puntosDisponibles = new List<GameObject>();
 
     void Start()
     {
-        GetGroundPositions();
-        SpawnFruits();
-    }
 
-    void GetGroundPositions()
-    {
-        groundPositions.Clear();
-
-        // Recorremos las celdas del Tilemap
-        foreach (var pos in groundTilemap.cellBounds.allPositionsWithin)
+        // Generar manzanas
+        int cantidadManzanas = Random.Range(1, maxManzanas + 1);
+        for (int i = 0; i < cantidadManzanas; i++)
         {
-            if (groundTilemap.HasTile(pos)) // hay un tile en esta celda
-            {
-                // convertir coordenada de celda a mundo
-                Vector3 worldPos = groundTilemap.GetCellCenterWorld(pos);
-                groundPositions.Add(worldPos);
-            }
+            GenerarFruta(manzanaPrefab);
+        }
+
+        // Generar bananas
+        int cantidadBananas = Random.Range(1, maxBananas + 1);
+        for (int i = 0; i < cantidadBananas; i++)
+        {
+            GenerarFruta(bananaPrefab);
         }
     }
 
-    void SpawnFruits()
+    void GenerarFruta(GameObject frutaPrefab)
     {
-        List<Vector3> availablePositions = new List<Vector3>(groundPositions);
+        if (puntosDisponibles.Count == 0) return;
 
-        int applesToSpawn = Random.Range(1, maxApples + 1);
-        int bananasToSpawn = Random.Range(1, maxBananas + 1);
+        int index = Random.Range(0, puntosDisponibles.Count);
+        GameObject punto = puntosDisponibles[index];
 
-        // Spawnear manzanas
-        for (int i = 0; i < applesToSpawn && availablePositions.Count > 0; i++)
-        {
-            int index = Random.Range(0, availablePositions.Count);
-            Vector3 pos = availablePositions[index] + Vector3.up * 0.5f; // un poquito arriba del piso
-            Instantiate(applePrefab, pos, Quaternion.identity);
-            availablePositions.RemoveAt(index);
-        }
+        Instantiate(frutaPrefab, punto.transform.position, Quaternion.identity);
 
-        // Spawnear bananas
-        for (int i = 0; i < bananasToSpawn && availablePositions.Count > 0; i++)
-        {
-            int index = Random.Range(0, availablePositions.Count);
-            Vector3 pos = availablePositions[index] + Vector3.up * 0.5f;
-            Instantiate(bananaPrefab, pos, Quaternion.identity);
-            availablePositions.RemoveAt(index);
-        }
+        puntosDisponibles.RemoveAt(index);
     }
 }
-
